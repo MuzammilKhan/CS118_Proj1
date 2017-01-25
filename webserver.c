feaa3294756e9332f2a326f19b73fd54998d040c
Muzammil Khan <muzammil.a.khan@live.com>
2017-01-25 15:34:13 -0800

    fixed bug: here is the message doesnt repeat with a blank line anymore

--- webserver.c
+++ webserver.c
@@ -15,77 +15,65 @@ void error(char *msg)
 
 int main(int argc, char *argv[])
 {
-    int sockfd, newsockfd, portno, pid;
-    fd_set active_fd_set; //set that holds sockets
+  int sockfd, newsockfd, portno, pid;
+  socklen_t clilen;
+  struct sockaddr_in serv_addr, cli_addr;
+  fd_set active_fd_set; // set for holding sockets
 
-    socklen_t client_len;
-    struct sockaddr_in serv_addr, client_addr;
-
-    if (argc < 2) {
-        fprintf(stderr,"ERROR, no port provided\n");
-        exit(1);
+  if (argc < 2) {
+      fprintf(stderr,"ERROR, no port provided\n");
+      exit(1);
+  }
+  sockfd = socket(AF_INET, SOCK_STREAM, 0);  //create socket
+  if (sockfd < 0) 
+    error("ERROR opening socket");
+  memset((char *) &serv_addr, 0, sizeof(serv_addr)); //reset memory
+  //fill in address info
+  portno = atoi(argv[1]);
+  serv_addr.sin_family = AF_INET;
+  serv_addr.sin_addr.s_addr = INADDR_ANY;
+  serv_addr.sin_port = htons(portno);
+   
+  if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) 
+    error("ERROR on binding");
+     
+  listen(sockfd,5);  //5 simultaneous connection at most
+     
+  /* Initialize the set of active sockets */
+  FD_ZERO(&active_fd_set);
+  /* put sock to the set, s.t. we can monitor whether a new connection request arrives */
+  FD_SET(sockfd, &active_fd_set);
+  while (1) {
+    /* Block until some sockets are active. Let N is #sockets+1 in active_fd_set */
+    if (select(sockfd + 1, &active_fd_set, NULL, NULL, NULL) < 0) {
+      exit(-1); // error
     }
-
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);   //create socket
-    if (sockfd < 0) 
-       error("ERROR opening socket");
-
-    /*initialize the set of active sockets */
-    FD_ZERO(&active_fd_set);
-
-    /*put sockfd to the set, so that we can monitor
-    if a new connection request arrives */
-    FD_SET(sockfd, &active_fd_set); 
-
-    memset((char *) &serv_addr, 0, sizeof(serv_addr));  //reset memory
-    //fill in address info
-    portno = atoi(argv[1]);
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = INADDR_ANY;
-    serv_addr.sin_port = htons(portno);
-     
-     if (bind(sockfd, (struct sockaddr *) &serv_addr,
-        sizeof(serv_addr)) < 0) 
-        error("ERROR on binding");
-     
-    listen(sockfd,5);   //5 simultaneous connection at most
-     
-    while(1) { //Keep listening for clients
-      /* Block until some sockets are active. Let N is #sockets+1 in active_fd_set*/
-      if(select (sockfd+1, &active_fd_set, NULL, NULL, NULL)<0) {exit(-1);} //exit if error   
-
-      /* Now some of the sockets are active */
-      if(FD_ISSET(sockfd, &active_fd_set)){ //new connection request
-        newsockfd = accept (sockfd, (struct sockaddr*)&client_addr, &client_len);
-          //if (newsockfd < 0) 
-            //error("ERROR on accept");
-        FD_SET(newsockfd, &active_fd_set);
-      }
-      /* Decide whether client has sent data */
-      if (FD_ISSET(newsockfd, &active_fd_set)) {
-        /* receive and process the data*/      
-        int n;
-        char buffer[256]; //TODO: Make this buffer size bigger? how much?
-             
-        memset(buffer, 0, 256); //reset memory
+    /* Now some sockets are active */
+    if (FD_ISSET(sockfd, &active_fd_set)) { // new connection request
+      newsockfd = accept(sockfd, (struct sockaddr*) &cli_addr, &clilen);
+      FD_SET(newsockfd, &active_fd_set);
+    }
+    /* Decide whether client has sent data */
+    if (FD_ISSET(newsockfd, &active_fd_set)) {
+      /* receive and process data here */
+      int n;
+      char buffer[256];
           
-        //read client's message
-        n = read(newsockfd,buffer,255);
-        if (n < 0){ error("ERROR reading from socket");}
-        else{ printf("Here is the message: %s\n",buffer);} //prints twice later on, maybe one of those is TCP and the other is the file?
-         
-        //reply to client
-        n = write(newsockfd,"I got your message",18);
-        if (n < 0) error("ERROR writing to socket");
-        /*TODO: Check where to put these closes*/        
-        close(newsockfd);//close connection 
-      }
+      memset(buffer, 0, 256);    //reset memory
+      
+      //read client's message
+      n = read(newsockfd,buffer,255);
+      if (n < 0) error("ERROR reading from socket");
+      printf("Here is the message: %s\n",buffer);
+     
+      //reply to client
+      n = write(newsockfd,"I got your message",18);
+      if (n < 0) error("ERROR writing to socket");
     }
+  } // end of while
+   
+  close(newsockfd);//close connection 
+  close(sockfd);
   
-
-    /*TODO: Check where to put these closes*/        
-    close(sockfd);
-         
-    return 0; 
+  return 0; 
 }
-
