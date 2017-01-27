d2dcd201038f86a4b68e9cc9b1dfb4add9ce263f
Muzammil Khan <muzammil.a.khan@live.com>
2017-01-26 23:12:54 -0800

    added testfile and http response code. However, webserver.c is broken and needs to be fixed.

--- webserver.c
+++ webserver.c
@@ -6,11 +6,114 @@
 #include <string.h>
 #include <sys/wait.h>   /* for the waitpid() system call */
 #include <signal.h> /* signal name macros, and the kill() prototype */
+#include <time.h> // for date time when return http response
 
 void error(char *msg)
 {
     perror(msg);
     exit(1);
+}
+
+// Generate http response for the http message and store it in response.
+// Returns: 0 on success
+//        -1 on error
+int httpResponse(char* message, char* response) {
+
+  //parse message to determine encoding type of message body
+  //assuming we always have a body for now TODO: Check if this is true and what to do if this is not so
+  int content_type = -1; // use this to determine content type, -1:error 0:html 1:gif 2:jpeg
+   //TODO:Check if we need any other types
+  char* filename = NULL;
+  filename = strtok(message, " /"); //Note: this approach apparently messes up message, but lets use it for now as we don't use message later
+  filename = strtok(filename, " /"); //now token holds the filename TODO: Check if this holds for weird filenames
+  if(strstr(filename , ".html") != NULL){
+    content_type = 0;
+  } else if(strstr(filename , ".gif") != NULL){
+    content_type = 1;
+  } else if(strstr(filename , ".jpeg") != NULL){
+    content_type = 2;
+  } else{
+    content_type = -1; //unkown content type, return error
+    return -1;
+  }
+  
+  //determine header content
+  int response_pos = 0;
+  //determine status
+  FILE *fp;
+  fp = fopen(filename, "r");
+  if(fp == NULL){ //file requested can't be found or error opening file
+    response[response_pos] = "HTTP/1.1 404 Not Found\r\n";
+    response_pos = response_pos + 24;
+  } else { //everything ok
+    response[response_pos] = "HTTP/1.1 200 OK\r\n";
+    response_pos = response_pos + 17;
+  }
+
+  //Date
+  //shown how to do this at: http://stackoverflow.com/questions/7548759/generate-a-date-string-in-http-response-date-format-in-c
+  char datetime[31];
+  time_t now = time(0);
+  struct tm tm = *gmtime(&now);
+  strftime(datetime, sizeof(datetime), "%a, %d %b %b %Y %H:%M:%S %Z", &tm);
+  datetime[29] = "\r\n";
+  strncpy(response + response_pos, datetime, 31);
+  response_pos = response_pos + 31;  
+
+  //Connection status
+  response[response_pos] = "Connection: close\r\n";
+  response_pos = response_pos + 19;
+
+  //Server field
+  //do we need one of these? ex: Server: Apache/1.2.27
+
+  //accept ranges
+  response[response_pos] = "Accept-Ranges: bytes\r\n"; //Todo: do we need another space?
+  response_pos = response_pos + 22;  
+
+  //content type
+  switch(content_type) {
+    case 0:
+      response[response_pos] = "Content-Type: text/html\r\n";
+      response_pos = response_pos + 25;  
+      break;
+    case 1:
+      response[response_pos] = "Content-Type: gif\r\n";
+      response_pos = response_pos + 19;  
+      break;
+    case 2:
+      response[response_pos] = "Content-Type: jpeg\r\n";
+      response_pos = response_pos + 20;  
+      break;
+    default:
+      break;
+  }
+
+  //Last modified field, do we need one?
+  //ex: Last-Modified: Thu, 26 Jan 2017 21:32:00 GMT
+
+  //empty line before message body TODO: check if necessary
+    response[response_pos] = "\r\n";
+    response_pos = response_pos + 2; 
+
+  //attach message body
+  int filechar;
+  while(1)
+  {
+    filechar = fgetc(fp);
+    if( feof(fp) )
+    {
+     break ;
+    }
+    response[response_pos] = filechar;
+    response_pos = response_pos + 1;
+  }  
+  fclose(fp);
+
+  //finish with /r/n/r/n ?
+  response[response_pos] = "\r\n\r\n";
+  response_pos = response_pos + 4; 
+  return 0;
 }
 
 int main(int argc, char *argv[])
@@ -72,12 +175,29 @@ int main(int argc, char *argv[])
       }
       printf("Here is the message: %s\n",buffer);
      
+      //Disable for part b? //TODO: double check this
+      /*
       //reply to client
       n = write(newsockfd,"I got your message",18);
       if (n < 0){
         close(newsockfd);//close connection
         close(sockfd);        
         error("ERROR writing to socket");
+      } */
+
+      int response_buffer_size = 2048; //TODO: Check how big this should be
+      char response_buffer[response_buffer_size];
+      if(httpResponse(buffer, response_buffer) < 0) {
+        close(newsockfd);//close connection
+        close(sockfd);
+        error("ERROR reading from socket");        
+      }
+
+      if(write(newsockfd, response_buffer, response_buffer_size)) {
+      //if(write(newsockfd, "HTTP/1.1 200 OK\nDate: Mon, 27 Jul 2009 12:28:53 GMT\nServer: Apache/2.2.14 (Win32)\nLast-Modified: Wed, 22 Jul 2009 19:15:56 GMT\nContent-Length: 88\nContent-Type: text/html\nConnection: Closed\n<html>\n<body>\n<h1>Hello, World!</h1>\n</body>\n</html>\n\n")) {
+        close(newsockfd);//close connection
+        close(sockfd);
+        error("ERROR reading from socket");
       }
     }
     close(newsockfd);//close connection  //TODO: CHECK IF THIS IS THE RIGHT LOCATION
